c99c7140246a883f4b97f64c6a1ed226c4fa0898
Riley Blackwell <rileyblackwell5@gmail.com>
2025-01-17 20:38:16 -0500

    Hello world msg

---
+++ cfe-saas/src/cfehome/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count(),
+    }
+    path = request.path
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
